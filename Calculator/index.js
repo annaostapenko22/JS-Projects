0397fd38d201972badfde9c096d4a9171315fba3
Anna Ostapenko <kvintabudinvest@gmail.com>
2019-12-08 20:02:56 +0200

    added final version

--- Calculator/index.js
+++ Calculator/index.js
@@ -5,85 +5,100 @@ const refs = {
   calculatorKeys: document.querySelector(".calculator-keys"),
   reset: document.querySelector(".resetBtn")
 };
-console.log(refs.reset);
+
 const calculator = {
   firstOperand: null,
   waitingForSecondOperand: false,
   operator: null,
-  value: "0",
-  sum(a, b) {
-    return a + b;
-  },
-  substruction(a, b) {
-    return a - b;
-  },
-  multiplication(a, b) {
-    return a * b;
-  },
-  division(a, b) {
-    return a / b;
-  }
+  value: "0"
+};
+
+const performCalculation = {
+  "+": (firstOperand, secondOperand) => firstOperand + secondOperand,
+  "-": (firstOperand, secondOperand) => firstOperand - secondOperand,
+  "*": (firstOperand, secondOperand) => firstOperand * secondOperand,
+  "/": (firstOperand, secondOperand) => firstOperand / secondOperand,
+  "=": (firstOperand, secondOperand) => secondOperand
 };
 
 function updateDisplay() {
   refs.output.value = calculator.value;
-  console.log(calculator);
 }
-// updateDisplay();
 
 function displayValue(evt) {
   if (evt.target.nodeName !== "BUTTON") {
     return;
   }
   if (evt.target.classList.contains("operator")) {
-    // console.log("operator", evt.target.value);
     handleOperator(evt.target.value);
     updateDisplay();
     return;
   }
   if (evt.target.classList.contains("period")) {
-    // console.log("period", evt.target.value);
     inputDecimal(evt.target.value);
     updateDisplay();
     return;
   }
   if (evt.target.classList.contains("resetBtn")) {
-    console.log("clear all", evt.target.value);
+    reset(evt.target.value);
+    updateDisplay();
     return;
   }
-  console.log("digit", evt.target.value);
   inputDigit(evt.target.value);
   updateDisplay();
 }
 
 function inputDigit(digit) {
   const value = calculator.value;
-  if (value === "0") {
+  if (calculator.waitingForSecondOperand === true) {
     calculator.value = digit;
+    calculator.waitingForSecondOperand = false;
   } else {
-    calculator.value = value + digit;
+    if (value === "0") {
+      calculator.value = digit;
+    } else {
+      calculator.value = value + digit;
+    }
   }
 }
 
 function inputDecimal(point) {
+  if (calculator.waitingForSecondOperand) {
+    return;
+  }
   if (!calculator.value.includes(point)) {
     calculator.value += point;
   }
 }
 
 function handleOperator(nextOperator) {
-  const { value, firstOperand, operator } = calculator;
-  const inputValue = parseFloat(value);
-  console.log(inputValue);
-  if (firstOperand === null) {
+  const inputValue = parseFloat(calculator.value);
+  if (calculator.operator && calculator.waitingForSecondOperand) {
+    calculator.operator = nextOperator;
+    return;
+  }
+
+  if (calculator.firstOperand === null) {
     calculator.firstOperand = inputValue;
+  }
+  if (calculator.operator) {
+    const result = performCalculation[calculator.operator](
+      calculator.firstOperand,
+      inputValue
+    );
+
+    calculator.value = String(result);
+    calculator.firstOperand = result;
   }
   calculator.waitingForSecondOperand = true;
   calculator.operator = nextOperator;
+}
 
-  console.log(operator);
+function reset() {
+  calculator.value = "0";
+  calculator.waitingForSecondOperand = false;
+  calculator.firstOperand = null;
+  calculator.operator = null;
 }
 
 refs.calculatorKeys.addEventListener("click", displayValue);
-// refs.reset.addEventListener("click", clearAll);
-// refs.output.addEventListener("change", updateDisplay);
