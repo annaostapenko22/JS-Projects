bcd851de18f526d930f89acac9d6dfde1e802077
Anna Ostapenko <kvintabudinvest@gmail.com>
2019-12-08 02:42:02 +0200

    added calculator methods

--- Calculator/index.js
+++ Calculator/index.js
@@ -2,19 +2,41 @@
 
 const refs = {
   output: document.querySelector(".outputField"),
-  calculatorKeys: document.querySelector(".calculator-keys")
+  calculatorKeys: document.querySelector(".calculator-keys"),
+  reset: document.querySelector(".resetBtn")
 };
+console.log(refs.reset);
 const calculator = {
-  value: "0"
+  value: "0",
+  sum(a, b) {
+    return a + b;
+  },
+  substruction(a, b) {
+    return a - b;
+  },
+  multiplication(a, b) {
+    return a * b;
+  },
+  division(a, b) {
+    return a / b;
+  }
 };
+
+refs.output.value = 0;
 
 function displayValue(evt) {
   if (evt.target.nodeName === "BUTTON") {
     calculator.value = evt.target.value;
-    let getNum = Number(evt.target.value);
-    refs.output.value = evt.target.value;
+    let getNum = parseFloat(evt.target.value);
+    console.log(getNum);
+    refs.output.value += evt.target.value;
   }
 }
 
+function clearAll() {
+  refs.output.value = 0;
+}
+
 refs.calculatorKeys.addEventListener("click", displayValue);
+refs.reset.addEventListener("click", clearAll);
 // refs.output.addEventListener("change", showValue);
