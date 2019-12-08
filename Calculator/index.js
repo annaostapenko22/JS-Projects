b472affb95bcc69479dee5f11c7fed58d5f0b947
Anna Ostapenko <kvintabudinvest@gmail.com>
2019-12-08 17:08:43 +0200

    added function handling with operands

--- Calculator/index.js
+++ Calculator/index.js
@@ -7,6 +7,9 @@ const refs = {
 };
 console.log(refs.reset);
 const calculator = {
+  firstOperand: null,
+  waitingForSecondOperand: false,
+  operator: null,
   value: "0",
   sum(a, b) {
     return a + b;
@@ -22,21 +25,65 @@ const calculator = {
   }
 };
 
-refs.output.value = 0;
+function updateDisplay() {
+  refs.output.value = calculator.value;
+  console.log(calculator);
+}
+// updateDisplay();
 
 function displayValue(evt) {
-  if (evt.target.nodeName === "BUTTON") {
-    calculator.value = evt.target.value;
-    let getNum = parseFloat(evt.target.value);
-    console.log(getNum);
-    refs.output.value += evt.target.value;
+  if (evt.target.nodeName !== "BUTTON") {
+    return;
+  }
+  if (evt.target.classList.contains("operator")) {
+    // console.log("operator", evt.target.value);
+    handleOperator(evt.target.value);
+    updateDisplay();
+    return;
+  }
+  if (evt.target.classList.contains("period")) {
+    // console.log("period", evt.target.value);
+    inputDecimal(evt.target.value);
+    updateDisplay();
+    return;
+  }
+  if (evt.target.classList.contains("resetBtn")) {
+    console.log("clear all", evt.target.value);
+    return;
+  }
+  console.log("digit", evt.target.value);
+  inputDigit(evt.target.value);
+  updateDisplay();
+}
+
+function inputDigit(digit) {
+  const value = calculator.value;
+  if (value === "0") {
+    calculator.value = digit;
+  } else {
+    calculator.value = value + digit;
   }
 }
 
-function clearAll() {
-  refs.output.value = 0;
+function inputDecimal(point) {
+  if (!calculator.value.includes(point)) {
+    calculator.value += point;
+  }
+}
+
+function handleOperator(nextOperator) {
+  const { value, firstOperand, operator } = calculator;
+  const inputValue = parseFloat(value);
+  console.log(inputValue);
+  if (firstOperand === null) {
+    calculator.firstOperand = inputValue;
+  }
+  calculator.waitingForSecondOperand = true;
+  calculator.operator = nextOperator;
+
+  console.log(operator);
 }
 
 refs.calculatorKeys.addEventListener("click", displayValue);
-refs.reset.addEventListener("click", clearAll);
-// refs.output.addEventListener("change", showValue);
+// refs.reset.addEventListener("click", clearAll);
+// refs.output.addEventListener("change", updateDisplay);
